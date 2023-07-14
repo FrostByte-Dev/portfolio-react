10fce24fa584b7e3cd148ae82a87b12f7430d97d
Jacques Levasseur <frostbyte-gaming@outlook.com>
2023-07-13 21:17:40 -0400

    Setup all routes in the react router.

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,20 @@ import { CssBaseline } from '@mui/material';
 import App from './App.tsx'
 import theme from './theme.tsx';
 import ErrorPage from './pages/ErrorPage/index.tsx';
+import Home from './pages/Home/index.tsx';
+import About from './pages/About/index.tsx';
+import Projects from './pages/Projects/index.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home/> },
+      { path: "/About", element: <About/> },
+      { path: "/Projects", element: <Projects/> }
+    ]
   }
 ], {
   basename: "/portfolio-react"
