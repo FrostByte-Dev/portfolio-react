8d55b55627c4f98be20a3ef2e93996792b5e9de6
Jacques Levasseur <frostbyte-gaming@outlook.com>
2023-07-13 21:18:18 -0400

    Created a component for all pages needed.

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+
+function About() {
+    return (
+        <Box>
+            <Box sx={{ my: 4 }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+                About Page
+            </Typography>
+            </Box>
+            <Box sx={{ my: 2 }}>
+            {[...new Array(12)]
+                .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+                        Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                        Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                )
+                .join('\n')}
+            </Box>
+        </Box>
+    );
+}
+
+export default About
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,27 @@
+import { Box, Typography } from "@mui/material"
+
+
+function Home() {
+    return (
+        <Box>
+            <Box sx={{ my: 4 }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+                Material UI Vite.js example in Typescript
+            </Typography>
+            </Box>
+            <Box sx={{ my: 2 }}>
+            {[...new Array(12)]
+                .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+                        Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                        Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                )
+                .join('\n')}
+            </Box>
+        </Box>
+    );
+    
+}
+
+export default Home
