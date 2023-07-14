cb204f4f7e619295f3672d952c226ae044f661da
Jacques Levasseur <frostbyte-gaming@outlook.com>
2023-07-13 21:17:17 -0400

    Updated App as base layout with an outlet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import TopBar from './components/TopBar';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <TopBar />
-      <Container maxWidth="sm">
-        <Box sx={{ my: 4}}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Material UI Vite.js example in Typescript
-          </Typography>
-        </Box>
+    <Box>
+      <Container maxWidth="lg">
+        <TopBar />
+        <Outlet />
       </Container>
     </Box>
   );
