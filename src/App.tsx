175809ef5b9bd4439b3c538789d1dc572f5fbbb0
Jacques Levasseur <frostbyte-gaming@outlook.com>
2023-07-18 21:45:02 -0400

    Moved profile card grid to Home page only.

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,17 @@ import Box from "@mui/material/Box";
 import TopBar from "./components/TopBar";
 import { Outlet } from "react-router-dom";
 import { Grid } from "@mui/material";
-import ProfileCard from "./components/ProfileCard";
 
 function App() {
   return (
     <Box m={2}>
-      <Grid container spacing={2} alignItems={"stretch"}>
+      <Grid container>
         <Grid item xs={12}>
           <TopBar />
         </Grid>
-        <Grid item lg={5} md={12} sm={12} xs={12}>
-          <ProfileCard />
-        </Grid>
-        <Grid item lg={7}>
-          <Outlet />
-        </Grid>
       </Grid>
+
+      <Outlet />
     </Box>
   );
 }
