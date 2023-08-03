78d161e0596c1245ed817b12ddfe567c636d3bc0
Jacques Levasseur <frostbyte-gaming@outlook.com>
2023-08-03 10:08:04 -0400

    Moved the styling to the sx tag.

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -1,47 +1,51 @@
-import { Box, Link, Stack, Typography } from "@mui/material";
+import { Grid, Link, Stack, Typography } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const ProfileCard = () => (
-  <Box
-    padding={2}
-    border={2}
-    borderTop={40}
-    borderRadius={2}
-    borderColor={"primary.main"}
-    color={"primary.main"}
+  <Grid
+    container
+    sx={{
+      padding: 2,
+      border: 2,
+      borderTop: 40,
+      borderRadius: 2,
+      borderColor: "primary.main",
+      bgcolor: "#fff",
+      color: "primary.main",
+    }}
   >
-    <Typography variant="h4" fontWeight={"bold"} gutterBottom>
-      Jacques Levasseur
-    </Typography>
-    <Typography variant="h6" fontWeight={"bold"} gutterBottom>
-      Senior Full Stack Developer
-    </Typography>
-    <Typography variant="body1" maxWidth={"25rem"}>
-      Passionate about delivering innovative products, I excel in
-      problem-solving and collaboration to enhance user experiences and drive
-      business growth.
-    </Typography>
-    <Stack direction={"row"} spacing={2} mt={2}>
-      {/* <a href="https://github.com/FrostByte-Dev" target="_blank">
-                <GitHubIcon />
-              </a> */}
-      <Link
-        href="https://github.com/FrostByte-Dev"
-        target="_blank"
-        color={"primary.main"}
-      >
-        <GitHubIcon />
-      </Link>
-      <Link
-        href="https://www.linkedin.com/in/jacqueslevasseur/"
-        target="_blank"
-        color={"primary.main"}
-      >
-        <LinkedInIcon />
-      </Link>
-    </Stack>
-  </Box>
+    <Grid item lg={8}>
+      <Typography variant="h4" fontWeight={"bold"} gutterBottom>
+        Jacques Levasseur
+      </Typography>
+      <Typography variant="h6" fontWeight={"bold"} gutterBottom>
+        Senior Full Stack Developer
+      </Typography>
+      <Typography variant="body1" maxWidth={"25rem"}>
+        Passionate about delivering innovative products, I excel in
+        problem-solving and collaboration to enhance user experiences and drive
+        business growth.
+      </Typography>
+      <Stack direction={"row"} spacing={2} mt={2}>
+        <Link
+          href="https://github.com/FrostByte-Dev"
+          target="_blank"
+          color={"primary.main"}
+        >
+          <GitHubIcon />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/jacqueslevasseur/"
+          target="_blank"
+          color={"primary.main"}
+        >
+          <LinkedInIcon />
+        </Link>
+      </Stack>
+    </Grid>
+    <Grid item lg={4}></Grid>
+  </Grid>
 );
 
 export default ProfileCard;
